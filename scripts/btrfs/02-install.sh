15b9bff2fd7357ddabd9af39b4c7f101d52e9737
Philipp <philipp@f1ndus.de>
2020-05-24 01:40:19 +0200

    added en_us locales

--- scripts/btrfs/02-install.sh
+++ scripts/btrfs/02-install.sh
@@ -48,6 +48,9 @@ echo "KEYMAP=de" > /mnt/etc/vconsole.conf
 sed -i 's/#\(de_DE.UTF-8\)/\1/' /mnt/etc/locale.gen
 echo 'LANG="de_DE.UTF-8"' > /mnt/etc/locale.conf
 
+sed -i 's/#\(en_US.UTF-8\)/\1/' /mnt/etc/locale.gen
+echo 'LANG="en_US.UTF-8"' >> /mnt/etc/locale.conf
+
 # Prepare initramfs
 print "Prepare initramfs"
 cat > /mnt/etc/mkinitcpio.conf <<"EOF"
